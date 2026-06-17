f62c89cdc4d3bbe5c65c5238dfe76782b61187d8
mikerpugh <mikerpugh@gmail.com>
2026-06-16 20:22:51 -0600

    Update streamlit_app.py
    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,9 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:  
         ingredients_string += fruit_chosen + ' ' 
 
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+  
     # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
@@ -46,8 +49,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response)
-st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
         
         
